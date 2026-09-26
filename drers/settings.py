a6998eef892980379d5bb1fa36adf23158163c50
thezishanansari <ansarizishan662@gmail.com>
2026-09-26 18:11:07 +0545

    error fixed

--- drers/settings.py
+++ drers/settings.py
@@ -130,15 +130,10 @@ MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 # ---------------------------------------------------------------------------
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-
 EMAIL_HOST = env("EMAIL_HOST", "smtp.gmail.com")
-
 EMAIL_PORT = int(env("EMAIL_PORT", 587))
-
 EMAIL_USE_TLS = True
-
 EMAIL_HOST_USER = env("EMAIL_HOST_USER", "")
-
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD", "")
 
 
